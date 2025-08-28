2ad735765d4ffb10a7971795ef7f5ec99abad307
adrien <adri.bonnivard@gmail.com>
2025-08-28 17:01:21 +0200

    build

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-const BASE = process.env.REACT_APP_API_BASE_URL || "http://localhost:3000";
+const BASE = process.env.REACT_APP_API_BASE_URL || "http://localhost:3001";
 
 // Requête générique **avec token**
 async function requestWithToken(path, options = {}) {
